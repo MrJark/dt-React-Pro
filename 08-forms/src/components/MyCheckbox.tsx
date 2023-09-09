e581ac3231fa2c1729af2fafb26b950159e509a7
Chema <mrjark7@gmail.com>
2023-09-09 12:33:09 +0200

    checkbox component

---
+++ 08-forms/src/components/MyCheckbox.tsx
@@ -0,0 +1,31 @@
+import { useField } from "formik"
+
+
+
+interface Props {
+    label: string, // este no es obligatorio
+    name: string, // obligatorio
+    [x: string]: any, // esto es un comodín que le podemos pasar como props en el cual hay una llave, x, que es siempre de tipo string y lo que recibe podrá ser cualquier cosa y elcual por defecto, lo hace opcional por tanto, no hace falta el ?
+}
+
+export const MyCheckbox = ({ label, ...props }: Props ) => { // desestructuración de las props
+
+    const [ field, meta ] = useField({...props, type: 'checkbox'});
+    // console.log({field, meta});
+    
+
+    return (
+        <>
+            <label >
+                <input type="checkbox" {...field} {...props} />
+                {label}
+            </label>
+
+            {
+                meta.touched && meta.error && (
+                    <span className="error">{meta.error}</span>
+                )
+            }
+        </>
+    )
+}
